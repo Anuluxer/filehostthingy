729e645bd54eabbd82fce65e308df38b0c3880c9
Anuluxer <95879548+Anuluxer@users.noreply.github.com>
2023-11-30 21:32:52 -0600

    Update nt64script.js

--- nt64script.js
+++ nt64script.js
@@ -1574,6 +1574,6 @@ window["Module"] = {
 
 var rando2 = Math.floor(Math.random() * 100000);
 var script2 = document.createElement('script');
-script2.src = 'https://cdn.jsdelivr.net/gh/Anuluxer/filehostthingy@main/input_controller.js?v=' + rando2;
+script2.src = 'https://cdn.jsdelivr.net/gh/Anuluxer/filehostthingy@latest/input_controller.js?v=' + rando2;
 document.getElementsByTagName('head')[0].appendChild(script2);
 
