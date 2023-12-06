fe3906fb46dd090b3fead0960d581e2c96096b5a
Anuluxer <95879548+Anuluxer@users.noreply.github.com>
2023-12-06 16:44:44 -0600

    Update update.js

--- update.js
+++ update.js
@@ -1,1 +1,11 @@
 var UPDATEVER = 1.0
+if (UPDATEVER > CURRVER) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("update").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "https://cdn.jsdelivr.net/gh/Anuluxer/filehostthingy@main/update.html", true);
+    xhttp.send();
+}
